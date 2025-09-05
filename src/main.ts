814f5d3173f19e9b5dcb56e21fc07235c1cbcea2
ThomasMoreno19 <thomasmoreno004@gmail.com>
2025-09-04 21:57:50 -0300

    Cambié el cors para que permita peticiones http de nuestro front

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Habilita CORS para el frontend
+  app.enableCors({
+    origin: 'https://2025-proyecto1-front-imc-drab.vercel.app',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(3000);
 }
