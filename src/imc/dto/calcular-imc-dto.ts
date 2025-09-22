40b3d9567090a0e98a51b09a26a0bff5a48f311f
Salvador Bacci <salvador_bacci@hotmail.com>
2025-09-22 18:21:14 -0300

    update: removed unnecessary Not-Null assertions

--- src/imc/dto/calcular-imc-dto.ts
+++ src/imc/dto/calcular-imc-dto.ts
@@ -9,7 +9,7 @@ export class CalcularImcDto {
   )
   @Min(0, { message: 'La altura debe ser mayor a 0 metros' }) // Altura mínima razonable
   @Max(3, { message: 'La altura no puede ser mayor a 3 metros' }) // Altura máxima razonable
-  altura!: number;
+  altura: number;
 
   @Type(() => Number)
   @IsNumber(
@@ -18,5 +18,5 @@ export class CalcularImcDto {
   )
   @Min(0, { message: 'El peso debe ser mayor a 0 kg' })
   @Max(500, { message: 'El peso no puede ser mayor a 500 kg' })
-  peso!: number;
+  peso: number;
 }
